670a56711789996e18a000c9db38da14d8c67b2a
itdevweb <miltoncorney.it@gmail.com>
2020-05-20 21:00:02 -0500

    ty-one

--- djecommerce/settings/production.py
+++ djecommerce/settings/production.py
@@ -13,11 +13,11 @@ AUTH_PASSWORD_VALIDATORS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('tylersdatabase_one'),
-        'USER': config('mcadmin'),
-        'PASSWORD': config('lollypop33'),
-        'HOST': config('localhost'),
-        'PORT': '3306'
+        'NAME': config('d3u85pr3rhajlb'),
+        'USER': config('egviqwdcbgawxg'),
+        'PASSWORD': config('47ec3907b02e0a1ecbe609f41cfb923cf4920206f27cc0dd727dc75790e965ff'),
+        'HOST': config('ec2-3-91-139-25.compute-1.amazonaws.com'),
+        'PORT': '5432'
     }
 }
 
