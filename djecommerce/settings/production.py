7001fcd78bd9ee4f51e17824e68451a1341cf54b
itdevweb <miltoncorney.it@gmail.com>
2020-05-24 15:45:20 -0500

    ty-one

--- djecommerce/settings/production.py
+++ djecommerce/settings/production.py
@@ -11,16 +11,29 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'}
 ]
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.path.join('d3u85pr3rhajlb'),
-        'USER': 'egviqwdcbgawxg',
-        'PASSWORD': '47ec3907b02e0a1ecbe609f41cfb923cf4920206f27cc0dd727dc75790e965ff',
-        'HOST': 'ec2-3-91-139-25.compute-1.amazonaws.com',
-        'PORT': '5432',
+        'NAME': config('d3u85pr3rhajlb'),
+        'USER': config('egviqwdcbgawxg'),
+        'PASSWORD': config('47ec3907b02e0a1ecbe609f41cfb923cf4920206f27cc0dd727dc75790e965ff'),
+        'HOST': config('ec2-3-91-139-25.compute-1.amazonaws.com'),
+        'PORT': '5432'
+    }
+}
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        #'NAME': os.path.join(BASE_DIR, 'tylersdatabase_one'),
+        'NAME': os.path.join('tylersdatabase_one'),
+        'USER': 'mcadmin',
+        'PASSWORD': 'lollypop33',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
 
     }
 }
