6cdae39eb5ed058508644272c56da0829ab81c0f
itdevweb <miltoncorney.it@gmail.com>
2020-05-24 13:38:10 -0500

    ty-one

--- djecommerce/settings/development.py
+++ djecommerce/settings/development.py
@@ -41,13 +41,17 @@ DEBUG_TOOLBAR_CONFIG = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('d3u85pr3rhajlb'),
-        'USER': config('egviqwdcbgawxg'),
-        'PASSWORD': config('47ec3907b02e0a1ecbe609f41cfb923cf4920206f27cc0dd727dc75790e965ff'),
-        'HOST': config('ec2-3-91-139-25.compute-1.amazonaws.com'),
-        'PORT': '5432'
+        #'NAME': os.path.join(BASE_DIR, 'tylersdatabase_one'),
+        'NAME': os.path.join('d3u85pr3rhajlb'),
+        'USER': 'egviqwdcbgawxg',
+        'PASSWORD': '47ec3907b02e0a1ecbe609f41cfb923cf4920206f27cc0dd727dc75790e965ff',
+        'HOST': 'ec2-3-91-139-25.compute-1.amazonaws.com',
+        'PORT': '5432',
+
+
     }
 }
+
 
 STRIPE_PUBLIC_KEY = ''
 STRIPE_SECRET_KEY = ''
